c2973c5ce6b05a7f667bb93b679423b7b80c1047
Vaibhav <vaibhavgorde5@gmail.com>
2025-02-07 06:02:39 +0000

    commit 5

--- myfile.java
+++ myfile.java
@@ -2,6 +2,6 @@ class myfile
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to jenkins server......");
+        System.out.println("Welcome to jenkins server");
     }
 }
